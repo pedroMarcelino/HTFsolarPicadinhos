c3a8ce370f39184cf300ae74dd63411489a464d0
Marcelino <pedro.marcelinodv@gmail.com>
2026-04-16 19:54:32 -0300

    base projetos com dependencias

---
+++ src/main/java/com/HTFsolarPicadinhos/HtFsolarPicadinhosApplication.java
@@ -0,0 +1,13 @@
+package com.HTFsolarPicadinhos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtFsolarPicadinhosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtFsolarPicadinhosApplication.class, args);
+	}
+
+}
